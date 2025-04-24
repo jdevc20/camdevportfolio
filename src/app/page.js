c41b2b849bb6018a0293375aa9ae724faa308b95
jdevc20 <jdcamarin20@gmail.com>
2025-04-24 11:12:46 +0800

    Redesign

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import ProfileSection from "./components/ProfileSection";
+
 import IntroductionSection from "./components/IntroductionSection";
-import TechIconsSection from "./components/TechIconsSection";
+import ProjectCard from "./components/ProjectCard";
+import ProjectSection from "./components/ProjectSection";
+
 
 export default function Home() {
   return (
     <div className="min-h-screen p-2 sm:p-4 max-w-6xl mx-auto">
       <IntroductionSection />
+      <ProjectSection/>
     </div>
   );
 }
